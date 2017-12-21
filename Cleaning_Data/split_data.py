834ee910fb70269d616bffc78ae3e51cbb67b0aa
juliennelachance <julienne.lachance@gmail.com>
2017-12-21 12:30:45 -0500

    File for splitting jsons into test/train/val sets.

--- Cleaning_Data/split_data.py
+++ Cleaning_Data/split_data.py
@@ -8,11 +8,11 @@ if __name__ == "__main__":
   this_dir = os.path.join(this_dir, 'data') 
 
   # The annotations file:
-  json_in = json.load(open('{:s}/final_merge_{:s}.json'.format(this_dir, 'vcocoDec'), 'r'))
+  json_in = json.load(open('{:s}/testout_{:s}.json'.format(this_dir, 'Dec18'), 'r'))
 
   # The data splits (lists of image indices):
-  vcoco_train = np.loadtxt(os.path.join(this_dir, 'vcoco_train.ids'))[:,np.newaxis]
-  vcoco_val = np.loadtxt(os.path.join(this_dir, 'vcoco_val.ids'))[:,np.newaxis]
+  vcoco_train = np.loadtxt(os.path.join(this_dir, 'vcoco_trainval.ids'))[:,np.newaxis]
+  #vcoco_val = np.loadtxt(os.path.join(this_dir, 'vcoco_val.ids'))[:,np.newaxis]
   vcoco_test = np.loadtxt(os.path.join(this_dir, 'vcoco_test.ids'))[:,np.newaxis]
 
 
@@ -21,7 +21,7 @@ if __name__ == "__main__":
   coco_imlist = [j_i['id'] for j_i in json_in['images']]	# image id list
   coco_imlist = np.array(coco_imlist)[:,np.newaxis]
 
-  print len(coco_imlist)
+  #print len(coco_imlist)
 
   in_vcoco = []
   for i in range(len(coco_imlist)):
@@ -29,7 +29,7 @@ if __name__ == "__main__":
       in_vcoco.append(i)
   j_images = [json_in['images'][ind] for ind in in_vcoco] 
 
-  print len(in_vcoco)
+  #print len(in_vcoco)
 
   # select annotations that we need
   coco_imlist = [j_i['image_id'] for j_i in json_in['annotations']] 	# image id list
@@ -50,39 +50,7 @@ if __name__ == "__main__":
 
 
 
-
-
-
-
-  json_in = json.load(open('{:s}/final_merge_{:s}.json'.format(this_dir, 'vcocoDec'), 'r'))
-
-  json_val = json_in
-  # select images that we need
-  coco_imlist = [j_i['id'] for j_i in json_in['images']]	# image id list
-  coco_imlist = np.array(coco_imlist)[:,np.newaxis]
-  in_vcoco = []
-  for i in range(len(coco_imlist)):
-    if np.any(coco_imlist[i] == vcoco_val):
-      in_vcoco.append(i)
-  j_images = [json_in['images'][ind] for ind in in_vcoco] 
-  print len(in_vcoco)
-
-  # select annotations that we need
-  coco_imlist = [j_i['image_id'] for j_i in json_in['annotations']] 	# image id list
-  coco_imlist = np.array(coco_imlist)[:,np.newaxis]
-  in_vcoco = []
-  for i in range(len(coco_imlist)):
-    if np.any(coco_imlist[i] == vcoco_val):
-      in_vcoco.append(i)
-  j_annotations = [json_in['annotations'][ind] for ind in in_vcoco] 
-
-  json_val['annotations'] = j_annotations
-  json_val['images'] = j_images
-
-
-
-
-  json_in = json.load(open('{:s}/final_merge_{:s}.json'.format(this_dir, 'vcocoDec'), 'r'))
+  json_in = json.load(open('{:s}/testout_{:s}.json'.format(this_dir, 'Dec18'), 'r'))
   json_test = json_in
   # select images that we need
   coco_imlist = [j_i['id'] for j_i in json_in['images']]	# image id list
@@ -92,7 +60,7 @@ if __name__ == "__main__":
     if np.any(coco_imlist[i] == vcoco_test):
       in_vcoco.append(i)
   j_images = [json_in['images'][ind] for ind in in_vcoco] 
-  print len(in_vcoco)
+  #print len(in_vcoco)
 
   # select annotations that we need
   coco_imlist = [j_i['image_id'] for j_i in json_in['annotations']] 	# image id list
@@ -114,17 +82,13 @@ if __name__ == "__main__":
 
 
 
-  vcoco = os.path.join(this_dir, 'vcoco_train1.json')
+  vcoco = os.path.join(this_dir, 'vcoco_trainval3.json')
   print("Writing COCO annotations needed for V-COCO (train) to %s."%(format(vcoco)))
   with open(vcoco, 'wt') as f:
     json.dump(json_train, f)
 
-  vcoco = os.path.join(this_dir, 'vcoco_val1.json')
-  print("Writing COCO annotations needed for V-COCO (val) to %s."%(format(vcoco)))
-  with open(vcoco, 'wt') as f:
-    json.dump(json_val, f)
 
-  vcoco = os.path.join(this_dir, 'vcoco_test1.json')
+  vcoco = os.path.join(this_dir, 'vcoco_test3.json')
   print("Writing COCO annotations needed for V-COCO (test) to %s."%(format(vcoco)))
   with open(vcoco, 'wt') as f:
     json.dump(json_test, f)
